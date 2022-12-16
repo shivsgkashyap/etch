f5940fdcba9b6baa7bf5ea3a6455fc23090f3291
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-16 18:52:18 +0000

    Coded and styled header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="navbar-items">
+      <img src="./src/images/etch.png" alt="Etch-a-sketch Logo" />
+      <h1>ETCH-A-SKETCH</h1>
+    </header>
+  );
+}
